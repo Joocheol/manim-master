b3636425b61c1af7a8aa5962bfa9cfc92cc5009d
Joocheol <joocheol@yonsei.ac.kr>
2020-07-22 07:17:21 +0900

    w

--- MyFiles/move_to_examples.py
+++ MyFiles/move_to_examples.py
@@ -4,9 +4,11 @@ from manimlib.imports import *
 class move_to_ex01(Scene):
     def construct(self):
 
-        for x in range()
+        for x in range(-7, 7):
             for y in [3, 2, -2, 0]:
                 c = Dot()
                 self.add(c)
+                c.move_to(np.array([x, y, 0]))
+                self.play(Write(c))
 
-                self.play(c.move_to, np.array([x, y, 0]))
+#                self.play(c.move_to, np.array([x, y, 0]))
