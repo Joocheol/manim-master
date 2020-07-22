c455aa83e43920eff7d8af323aec3b814492b386
Joocheol <joocheol@yonsei.ac.kr>
2020-07-23 07:41:00 +0900

    w

--- MyFiles/to_edge_example.py
+++ MyFiles/to_edge_example.py
@@ -3,7 +3,17 @@ from manimlib.imports import *
 
 class to_edge_example(Scene):
     def construct(self):
-        text = Text(r"\int x \, dx")
 
-        self.add(text)
-        self.play(text.shift, RIGHT*10, rate_func=there_and_back, run_time=10)
+        c = Circle()
+        vt = ValueTracker(0)
+
+        # self.add(c)
+        number = DecimalNumber(-5, color=RED).next_to(c, UP)
+
+        number.add_updater(lambda m: m.next_to(c, UP))
+        number.add_updater(lambda m: m.set_value(vt.get_value()))
+        self.add(number)
+        self.play(c.shift, RIGHT * 10, rate_func=there_and_back, run_time=10)
+        self.play(vt.set_value, 10, rate_func=linear, run_time=10)
+        self.play(c.shift, RIGHT * -10, rate_func=there_and_back, run_time=10)
+        self.wait()
