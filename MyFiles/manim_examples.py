c85e3ca079e1ed15ac634234fa48a8485e722050
Joocheol <joocheol@yonsei.ac.kr>
2020-07-25 05:42:39 +0900

    w

--- MyFiles/manim_examples.py
+++ MyFiles/manim_examples.py
@@ -2,4 +2,12 @@ from manimlib.imports import *
 
 class manim_examples(Scene):
     def construct(self):
+
+        c = Circle()
+        d = Dot()
+
+        g = VGroup(c, d)
+
+        self.add(g)
+        self.play(g.move_to, RIGHT*5, run_time=3)
         self.wait()
