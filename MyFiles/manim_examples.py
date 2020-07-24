b6b6503d022eca502908d474f2d55b1cc64427ee
Joocheol <joocheol@yonsei.ac.kr>
2020-07-25 08:29:31 +0900

    w

--- MyFiles/manim_examples.py
+++ MyFiles/manim_examples.py
@@ -6,6 +6,8 @@ class manim_examples(GraphScene):
         self.setup_axes()
 
         graph_1 = self.get_graph(lambda x: (1/2) * x ** 2 - 3)
+        d = Dot().move_to(self.coords_to_point(1, 2))
 
+        self.add(d)
         self.play(ShowCreation(graph_1))
         self.wait()
