2d7666490f5c09474b9584204cec35bcb1574812
Joocheol <joocheol@yonsei.ac.kr>
2020-07-27 14:17:18 +0900

    m

--- MyFiles/Transform_Coor.py
+++ MyFiles/Transform_Coor.py
@@ -9,3 +9,34 @@ class ExampleTransformation(LinearTransformationScene):
         self.add_vector(np.array(TRANFORMED_VECTOR).flatten())
         self.apply_matrix(EXAMPLE_TRANFORM)
         self.wait()
+
+class LinearAlgebraIntuitions(Scene):
+    def construct(self):
+        title = TextMobject("Preview of core visual intuitions")
+        title.to_edge(UP)
+        h_line = Line(FRAME_X_RADIUS*LEFT, FRAME_X_RADIUS*RIGHT)
+        h_line.next_to(title, DOWN)
+        h_line.set_color(BLUE_E)
+        intuitions = [
+            "Matrices transform space",
+            "Matrix multiplication corresponds to applying " +
+            "one transformation after another",
+            "The determinant gives the factor by which areas change",
+        ]
+
+        self.play(
+            Write(title),
+            ShowCreation(h_line),
+            run_time = 2
+        )
+
+        for count, intuition in enumerate(intuitions, 3):
+            intuition += " (details coming in chapter %d)"%count
+            mob = TextMobject(intuition)
+            mob.scale(0.7)
+            mob.next_to(h_line, DOWN)
+            self.play(FadeIn(mob))
+            self.wait(4)
+            self.play(FadeOut(mob))
+            self.remove(mob)
+        self.wait()
