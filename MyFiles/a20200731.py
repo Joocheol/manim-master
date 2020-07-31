07167365e893598ad369ea025198564eefedda96
Joocheol <joocheol@yonsei.ac.kr>
2020-07-31 16:10:52 +0900

    m

--- MyFiles/a20200731.py
+++ MyFiles/a20200731.py
@@ -1,43 +1,35 @@
 from manimlib.imports import *
 
 
-class a2020073105(Scene):
-    def construct(self):
-        text_01 = TextMobject(
-            r'''$n$ linear equations, $n$ unknowns'''
-        )
+class a2020073112(GraphScene):
+    CONFIG = {
+        "x_min": -FRAME_X_RADIUS,
+        "x_max": FRAME_X_RADIUS,
+        "x_axis_width": FRAME_WIDTH,
+        "x_leftmost_tick": int(-FRAME_X_RADIUS),
+        "y_min": -FRAME_Y_RADIUS,
+        "y_max": FRAME_Y_RADIUS,
+        "y_axis_height": FRAME_HEIGHT,
+        "graph_origin": ORIGIN,
+    }
 
-        text_02 = TextMobject(
-            r'''Row picture'''
-        )
-        text_02.next_to(text_01, DOWN)
-
-        text_03 = TextMobject(
-            r'''(*) Column picture'''
-        )
-        text_03.next_to(text_02, DOWN)
-
-        text_04 = TextMobject(
-            r'''Matrix form'''
-        )
-        text_04.next_to(text_03, DOWN)
-
-        vg_1 = VGroup(text_01, text_02, text_03, text_04)
-        vg_1.move_to(ORIGIN)
-
-        self.play(Write(vg_1))
-
-
-class a2020073106(Scene):
     def construct(self):
         formula_1 = TexMobject(r'''
-            2x  - y = 0 \\
-            -x  + 2y = 3
+                            2x  - y = 0 \\
+                            -x  + 2y = 3
+                        ''')
+
+        formula_1.to_corner(UL, buff=LARGE_BUFF)
+        self.add(formula_1)
+
+        formula_2 = TexMobject(r'''
+        x \begin{bmatrix} 2 \\ -1 \end{bmatrix}
+        + y \begin{bmatrix} -1 \\ 2 \end{bmatrix}
+        = \begin{bmatrix} 0 \\ 3 \end{bmatrix}
         ''')
 
-        self.play(Write(formula_1))
-
-        self.play(formula_1[0][12].move_to, TOP)
+        self.add(formula_2[0][5])
+        self.wait()
 
 
 class a2020073111(GraphScene):
@@ -89,31 +81,41 @@ class a2020073111(GraphScene):
         self.play(Write(num_1), Write(num_2))
         self.wait()
 
-class a2020073112(GraphScene):
-    CONFIG = {
-        "x_min": -FRAME_X_RADIUS,
-        "x_max": FRAME_X_RADIUS,
-        "x_axis_width": FRAME_WIDTH,
-        "x_leftmost_tick": int(-FRAME_X_RADIUS),
-        "y_min": -FRAME_Y_RADIUS,
-        "y_max": FRAME_Y_RADIUS,
-        "y_axis_height": FRAME_HEIGHT,
-        "graph_origin": ORIGIN,
-    }
+
+class a2020073106(Scene):
     def construct(self):
         formula_1 = TexMobject(r'''
-                            2x  - y = 0 \\
-                            -x  + 2y = 3
-                        ''')
-
-        formula_1.to_corner(UL, buff=LARGE_BUFF)
-        self.add(formula_1)
-
-        formula_2 = TexMobject(r'''
-        x \begin{bmatrix} 2 \\ -1 \end{bmatrix}
-        + y \begin{bmatrix} -1 \\ 2 \end{bmatrix}
-        = \begin{bmatrix} 0 \\ 3 \end{bmatrix}
+            2x  - y = 0 \\
+            -x  + 2y = 3
         ''')
 
-        self.add(formula_2[0][5])
-        self.wait()
+        self.play(Write(formula_1))
+
+        self.play(formula_1[0][12].move_to, TOP)
+
+
+class a2020073105(Scene):
+    def construct(self):
+        text_01 = TextMobject(
+            r'''$n$ linear equations, $n$ unknowns'''
+        )
+
+        text_02 = TextMobject(
+            r'''Row picture'''
+        )
+        text_02.next_to(text_01, DOWN)
+
+        text_03 = TextMobject(
+            r'''(*) Column picture'''
+        )
+        text_03.next_to(text_02, DOWN)
+
+        text_04 = TextMobject(
+            r'''Matrix form'''
+        )
+        text_04.next_to(text_03, DOWN)
+
+        vg_1 = VGroup(text_01, text_02, text_03, text_04)
+        vg_1.move_to(ORIGIN)
+
+        self.play(Write(vg_1))
