14f7516aa26ee35326ecdb619b2452ca1c19b290
Joocheol <joocheol@yonsei.ac.kr>
2020-08-01 12:33:17 +0900

    m

--- MyFiles/Right_Multiplication.py
+++ MyFiles/Right_Multiplication.py
@@ -1,13 +1,113 @@
 from manimlib.imports import *
 
+class a2020080111(Scene):
+    def construct(self):
+
+        rec = Rectangle(height=0.1, width=0.1).set_fill(YELLOW, opacity=0.8).set_stroke(width=0)
+        g = VGroup(*[rec.copy() for i in range(6)]).arrange(RIGHT)
+        gg = VGroup(*[g.copy() for i in range(10)]).arrange(DOWN)
+
+        self.add(gg)
+        self.add_brackets(gg)
+        self.wait()
+
+        rec_2 = Rectangle(height=0.4, width=2.5).set_fill(YELLOW, opacity=0.8).set_stroke(width=0)
+        self.play(TransformFromCopy(gg, rec_2))
+
+        rec_3 = Rectangle(height=2.5, width=0.4).set_fill(YELLOW, opacity=0.8).set_stroke(width=0)
+        self.play(TransformFromCopy(rec_2, rec_3))
+
+    def add_brackets(self, mobj):
+        bracket_pair = TexMobject("\\big[", "\\big]")
+        bracket_pair.scale(2)
+        bracket_pair.stretch_to_fit_height(
+            mobj.get_height() + 2 * 0.1
+        )
+        l_bracket, r_bracket = bracket_pair.split()
+        l_bracket.next_to(mobj, LEFT, .2)
+        r_bracket.next_to(mobj, RIGHT, .2)
+        self.add(l_bracket, r_bracket)
+        self.brackets = VGroup(l_bracket, r_bracket)
+        return self
+
+class a2020080110(Scene):
+    def construct(self):
+
+        # self.right()
+        self.left()
+
+    def left(self):
+        m12 = Matrix([["-", "-"]])
+        m22 = Matrix([["-", "-"], ["-", "-"]])
+        m32 = Matrix([["-", "-"], ["-", "-"], ["-", "-"]])
+        m42 = Matrix([["-", "-"], ["-", "-"], ["-", "-"], ["-", "-"]])
+        g = VGroup(m12, m22, m32, m42).arrange()
+        self.play(Write(g))
+
+    def right(self):
+        m21 = Matrix([["-"], ["-"]])
+        m22 = Matrix([["-", "-"], ["-", "-"]])
+        m23 = Matrix([["-", "-", "-"], ["-", "-", "-"]])
+        m24 = Matrix([["-", "-", "-", "-"], ["-", "-", "-", "-"]])
+        g = VGroup(m21, m22, m23, m24).arrange()
+        self.play(Write(g))
+
+
+class a2020080109(Scene):
+    def construct(self):
+        m_1 = Matrix([[1, 2], [3, 4]])
+        self.play(Write(m_1))
+
+        b_1 = Brace(m_1, LEFT)
+        self.play(ShowCreation(b_1))
+
+        num_1 = TexMobject("2")
+        num_1.next_to(b_1, LEFT)
+        self.play(Write(num_1))
+
+        b_2 = Brace(m_1, UP)
+        self.play(ShowCreation(b_2))
+
+        num_2 = TexMobject("2")
+        num_2.next_to(b_2, UP)
+        self.play(Write(num_2))
+
+        f_1 = TexMobject(r"2 \times 2")
+        f_1.next_to(m_1, 2*DOWN)
+
+        self.play(TransformFromCopy(num_1, f_1[0][0]))
+        self.play(TransformFromCopy(num_2, f_1[0][2]))
+        self.add(f_1)
+        self.play(
+            FadeOut(b_1),
+            FadeOut(b_2),
+            FadeOut(num_1),
+            FadeOut(num_2),
+        )
+
+        f_2 = TexMobject(r"2 \times n")
+        f_2.next_to(f_1, 5 * RIGHT)
+        self.play(Write(f_2), f_2[0][0].set_color, "#FF00FF", f_1[0][2].set_color, YELLOW)
+
+        f_3 = TexMobject(r"m \times 2")
+        f_3.next_to(f_1, 5 * LEFT)
+        self.play(Write(f_3), f_3[0][2].set_color, "#00FF00", f_1[0][0].set_color, "#0000FF")
+
+
+
+
+
 class a2020080105(Scene):
     def construct(self):
 
-        r_1 = Matrix([[1, 2], [3, 4]])
+        d = Circle()
+        r_1 = Matrix([[d, "-"], [3, 4]])
         r_2 = Matrix([[1, 2], [3, 5]])
-        r_3 = VGroup(r_1, r_2).arrange()
+        t_1 = TexMobject(r"a \times ")
+        r_3 = VGroup(r_1, t_1, r_2).arrange()
 
-        self.play(Write(r_3[0][0][0]), Write(r_3[0][0][2]))
+        # self.play(Write(r_3[0][0][0]), Write(r_3[0][0][2]))
+        self.play(Write(r_3))
         self.wait()
 
         v_1 = VGroup(r_3[0][0][0], r_3[0][0][2])
