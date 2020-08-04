c96715b578147dd5315885989443e241fc73937a
Joocheol <joocheol@yonsei.ac.kr>
2020-08-04 15:24:29 +0900

    m

--- MyFiles/a20200804.py
+++ MyFiles/a20200804.py
@@ -9,20 +9,20 @@ class a2020080406(Scene):
         self.play(Write(t_1))
         self.wait()
 
-        v_1 = TexMobject(r"\bold{v} &= (3, -i) \\ \bold{w} &= (2+i, 4)")
+        v_1 = TexMobject(r"\bold{v} &= (3, -i) \\ \bold{w} &= (2+i, 4)").scale(0.7)
         v_2 = TexMobject(
             r"""\bra{v} &= \begin{bmatrix} 3 & i \end{bmatrix} \\ 
             \ket{w} &= \begin{bmatrix} 2+i \\ 4 \end{bmatrix}"""
-        )
-        g_1 = VGroup(v_1, v_2).arrange(4*RIGHT).move_to(2*UP)
+        ).scale(0.7)
+        g_1 = VGroup(v_1, v_2).arrange(4*RIGHT).move_to(UP)
 
         self.play(Write(g_1))
         self.wait()
 
         v_3 = TexMobject(
             r"\braket{v}{w} = 3 ( 2 + i ) + 4i = 6 + 7i"
-        )
-        v_3.next_to(g_1, 2*DOWN)
+        ).scale(0.7)
+        v_3.next_to(g_1, 3*DOWN)
 
         self.play(Write(v_3))
         self.wait()
@@ -32,8 +32,8 @@ class a2020080406(Scene):
             = \begin{bmatrix} (2+i) 3 & (2+i) i \\ 4\cdot 3 & 4i \end{bmatrix}
             = \begin{bmatrix} 6+3i & 2i - 1 \\ 12 & 4i \end{bmatrix}
             """
-        )
-        v_4.next_to(v_3, 2 * DOWN)
+        ).scale(0.7)
+        v_4.next_to(v_3, 3 * DOWN)
 
         self.play(Write(v_4))
         self.wait()
