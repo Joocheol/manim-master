666f9c2a58a3b946523ae5e8b7916c5a42959cb2
Joocheol <joocheol@yonsei.ac.kr>
2020-08-16 21:08:38 +0900

    a

---
+++ MyFiles/Derivatives/Transaction.py
@@ -0,0 +1,46 @@
+from manimlib.imports import *
+
+
+class intro(Scene):
+    def construct(self):
+        text_01 = TextMobject("Present").move_to(LEFT*3 + UP*3)
+        text_02 = TextMobject("Future").move_to(RIGHT*3 + UP*3)
+        formula_01 = TexMobject(r"\$ 10").move_to(LEFT*3 + UP*1)
+        formula_02 = TexMobject(r"\$ 11").move_to(RIGHT*3 + UP*1)
+        formula_03 = TexMobject(r"P_0").move_to(LEFT*3 + UP*-1)
+        formula_04 = TexMobject(r"P_T").move_to(RIGHT*3 + UP*-1)
+        line_01 = Line(start=TOP, end=BOTTOM)
+        line_02 = Line(start=LEFT*2.5, end=RIGHT*2.5).move_to([0, 1, 0])
+        line_03 = Line(start=LEFT * 2.5, end=RIGHT * 2.5).move_to([0, -1, 0])
+        arrow_01 = DoubleArrow(formula_01.get_center(), formula_03)
+
+        self.add(text_01)
+        self.add(text_02)
+        self.add(line_01)
+
+        self.play(
+            Write(formula_01),
+            Write(formula_02)
+        )
+        self.wait()
+
+        self.play(
+            Transform(line_01, line_02)
+        )
+        self.wait()
+
+        self.play(
+            Write(formula_03),
+            Write(formula_04)
+        )
+        self.wait()
+
+        self.play(
+            ShowCreation(line_03)
+        )
+        self.wait()
+
+        self.play(
+            ShowCreation(arrow_01)
+        )
+        self.wait()
