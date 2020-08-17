fc592b0487e82f1e45fc834082baf2622ae043b7
Joocheol <joocheol@yonsei.ac.kr>
2020-08-17 21:04:10 +0900

    a

--- MyFiles/Derivatives/Transaction.py
+++ MyFiles/Derivatives/Transaction.py
@@ -9,10 +9,13 @@ class intro(Scene):
         formula_02 = TexMobject(r"\$ 11").move_to(RIGHT*3 + UP*1)
         formula_03 = TexMobject(r"S_0").move_to(LEFT*3 + UP*-2)
         formula_04 = TexMobject(r"S_T").move_to(RIGHT*3 + UP*-2)
-        line_01 = Line(start=TOP, end=BOTTOM)
-        line_02 = Line(start=LEFT*2.5, end=RIGHT*2.5).move_to([0, 1, 0])
-        line_03 = Line(start=LEFT * 2.5, end=RIGHT * 2.5).move_to([0, -1, 0])
-        arrow_01 = DoubleArrow(formula_04.get_top(), formula_02.get_bottom())
+        line_01 = DashedLine(start=TOP, end=BOTTOM)
+        # line_02 = Line(start=LEFT*2.5, end=RIGHT*2.5).move_to([0, 1, 0])
+        # line_03 = Line(start=LEFT * 2.5, end=RIGHT * 2.5).move_to([0, -1, 0])
+        arrow_01 = DoubleArrow(formula_03.get_top(), formula_01.get_bottom())
+        arrow_02 = DoubleArrow(formula_03.get_top(), formula_02.get_bottom())
+        arrow_03 = DoubleArrow(formula_04.get_top(), formula_01.get_bottom())
+        arrow_04 = DoubleArrow(formula_04.get_top(), formula_02.get_bottom())
 
         self.add(text_01)
         self.add(text_02)
@@ -20,27 +23,16 @@ class intro(Scene):
 
         self.play(
             Write(formula_01),
-            Write(formula_02)
-        )
-        self.wait()
-
-        self.play(
-            Transform(line_01, line_02)
-        )
-        self.wait()
-
-        self.play(
+            Write(formula_02),
             Write(formula_03),
             Write(formula_04)
         )
         self.wait()
 
         self.play(
-            ShowCreation(line_03)
-        )
-        self.wait()
-
-        self.play(
-            ShowCreation(arrow_01)
+            ShowCreation(arrow_01),
+            ShowCreation(arrow_02),
+            ShowCreation(arrow_03),
+            ShowCreation(arrow_04)
         )
         self.wait()
